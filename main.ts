1a1d6f24f78f8fafb3f336da25ed5715acbad17a
ariannae2222 <93997204+ariannae2222@users.noreply.github.com>
2021-11-10 09:52:30 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Umbrella)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Umbrella)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Umbrella)
+})
